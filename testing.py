519dd374dde51d379c4d5fbf587659c36a69dc3d
Daniel Levitin <dlevitin@wisc.edu>
2024-03-27 09:49:12 +0100

    demonstrating source code bug

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@ import unittest
 from rips_horosphere_generator import RipsHorosphereGenerator
 from divergence_horosphere_generator import DivergenceHorosphereGenerator
 from defining_data import WeirdGroupData, SierpinskiCarpetData, VirtualSurfaceData
+from sage.combinat.finite_state_machine import FSMTransition, Automaton
 
 # To run: 
 # sage --python3 -m unittest testing.py
@@ -15,9 +16,31 @@ class BasicTestSuite(unittest.TestCase):
 
     def test_virtual_surface_divergence_graph_mockup(self):
         #I am getting a strange error in processing a specific stage in test_virtual_surface_divergence_graph.
+
+        '''
         Data = VirtualSurfaceData()
         HorosphereGenerator = DivergenceHorosphereGenerator(Data.c_map, Data.o_map, Data.ray)
         SuspectMachine = HorosphereGenerator.same_length_edge_checker1256
+        '''
+        #This transition list mocks up part of the machine that appears in the above comment.
+        TransitionList = [FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (4, 4), True), ('a', 'a')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (4, 4), True), ('e', 'e')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (4, 4), True), ('c', 'c')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ('b', 'b')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (4, 4), True), ('d', 'd')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('e',), ((('e',), ('a', 'd')),), ('a',), ('b', 'e'), (4, 4), True), ('a', 'e')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('a',), ((('a',), ('b', 'e')),), ('b',), ('a', 'c'), (4, 1), False), ('a', 'b')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('e',), ((('e',), ('a', 'd')),), ('a',), ('b', 'e'), (4, 4), False), ('e', 'a')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('e',), ((('e',), ('a', 'd')),), ('d',), ('c', 'e'), (4, 4), False), ('e', 'd')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('c',), ((('c',), ('b', 'd')),), ('b',), ('a', 'c'), (4, 1), False), ('c', 'b')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('d',), ((('d',), ('c', 'e')),), ('c',), ('b', 'd'), (4, 4), True), ('c', 'd')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('a',), ((('a',), ('b', 'e')),), ('b',), ('a', 'c'), (1, 4), True), ('b', 'a')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('c',), ((('c',), ('b', 'd')),), ('b',), ('a', 'c'), (1, 4), True), ('b', 'c')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('e',), ((('e',), ('a', 'd')),), ('d',), ('c', 'e'), (4, 4), True), ('d', 'e')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), ('d',), ((('d',), ('c', 'e')),), ('c',), ('b', 'd'), (4, 4), False), ('d', 'c')),
+        FSMTransition(((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True), ((), (), (), (), (), (), (), (), (), ('a', 'b', 'c', 'd', 'e'), (4, 4), True, 'final'), ('-', '-'))]
+        SuspectMachine = Automaton(TransitionList, initial_states = [((), (), (), (), (), (), (), (), (), ('a', 'e', 'c', 'b', 'd'), (1, 1), True)])
+
         #Check that there are no branches
         assert(SuspectMachine.is_deterministic())
         #Check that there are no epsilon transitions
@@ -33,6 +56,9 @@ class BasicTestSuite(unittest.TestCase):
         SecondState = SuspectMachine.process([('b', 'b')])[1]
         #In this version, we get the AttributeError 'FSMState' object has no attribute 'transitions'
         #SecondState = SuspectMachine.process([('b', 'b')], check_epsilon_transitions = False)[1]
+
+        '''
+        These lines do not run because of the above bug.
         SecondTransitionIter = SuspectMachine.iter_transitions(SecondState)
         SecondLabelList = [transition.word_in[0] for transition in SecondTransitionIter]
         assert(any(label == ('e', 'd') for label in SecondLabelList))
@@ -42,6 +68,7 @@ class BasicTestSuite(unittest.TestCase):
         assert(any(label == ('-', '-') for label in ThirdLabelList))
         #Check that the final state is accepted as is expected.
         assert(SuspectMachine.process( [('b', 'b'), ('e', 'd'), ('-', '-')])[0])
+        '''
 
         
 
